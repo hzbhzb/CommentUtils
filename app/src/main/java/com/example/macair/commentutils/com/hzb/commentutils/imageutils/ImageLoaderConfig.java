218c81ae198d997731373b2d9d063d77ee99150d
hzbhzb <1012734765@qq.com>
2016-04-02 13:26:03 +0800

    imageloder参数类

---
+++ app/src/main/java/com/example/macair/commentutils/com/hzb/commentutils/imageutils/ImageLoaderConfig.java
@@ -0,0 +1,10 @@
+package com.example.macair.commentutils.com.hzb.commentutils.imageutils;
+
+/**
+ * Created by huangzhbein on 16/4/1.
+ * 使用Buider模式构建Imageloder的参数，减少暴露过多的接口
+ */
+public class ImageLoaderConfig {
+
+
+}
